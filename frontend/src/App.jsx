c19adedca3b735c7a8ec86a66c7d3798bd349859
ARUNSANKAR S <arunsankars006@gmail.com>
2025-05-10 11:20:09 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,10 @@ import ImageUpload from './components/ImageUpload'
 
 function App() {
   return (
+    
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <ImageUpload />
+      
     </div>
   )
 }
