cf1ec9abb3555a42b55307b88ab9d1b6eeac54b3
ARUNSANKAR S <arunsankars006@gmail.com>
2025-05-13 23:39:04 +0530

    Create Loader.jsx

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Lottie from 'lottie-react';
+import loaderAnimation from '../assets/loader.json';
+
+const Loader = () => {
+  return (
+    <div className="loader-overlay">
+      <Lottie animationData={loaderAnimation} loop={true} />
+    </div>
+  );
+};
+
+export default Loader;
